adc68606680f4ceb78d6938fad65da4187c5cdd7
Kipkosgei Daniel <85983789+Chirchir-Dan@users.noreply.github.com>
2026-04-27 21:43:45 +0200

    added word categories to SuggestWordModal

--- components/shared/SuggestWordModal.tsx
+++ components/shared/SuggestWordModal.tsx
@@ -192,6 +192,8 @@ export default function SuggestWordModal({ isOpen, onOpenChange, initialSearch, 
                       <option value="proverb">Kalewenet</option>
                       <option value="riddle">Tangoch</option>
                       <option value="saying">Saying</option>
+                      <option value="expression">Expression</option>
+                      <option value="number">Number</option>
                     </optgroup>
                   </select>
                   <div className="absolute right-5 top-1/2 -translate-y-1/2 pointer-events-none opacity-40">
