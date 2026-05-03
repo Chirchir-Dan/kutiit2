c2c645c40a8beb8688e0ea084df5044145bd7fab
Chirchir-Dan <1danchirchir@gmail.com>
2026-05-03 20:36:21 +0200

    decreased fuzzy search threshhold to 0.37

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -354,7 +354,7 @@ export default function AdminDashboard() {
         "plural_indefinite",
         "plural_definite"
       ],
-      threshold: 0.4,
+      threshold: 0.37,
       distance: 100,
     });
   }, [view, words, suggestions]);
